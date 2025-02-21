96fa00e791e9eb2f9809458fc3f1a98cb6a2e5eb
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-02-21 10:24:50 +0100

    xampp appache works

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- Import a file -> stop the script if problem return an error -->
+<?php require 'header.html';?>
+<!--  Import a file -> warning of issue, script continues to run -->
+
+
+
+
+<!-- Import a file -> stop the script if problem return an error -->
+<?php require 'footer.html';?>
+<!--  Import a file -> warning of issue, script continues to run -->
