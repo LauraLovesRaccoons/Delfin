436e8037dcb66b035ef11f260da7d8d5d2eaf2fc
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-05 16:00:37 +0100

    Update first_run.bat

--- batch files/first_run.bat
+++ batch files/first_run.bat
@@ -4,6 +4,7 @@ ECHO This Will Take A While
 ECHO --------------------------------
 cd ../
 docker compose --profile pma up -d --build
+composer install
 timeout /t 10
 docker compose --profile pma stop
 timeout /t 10
