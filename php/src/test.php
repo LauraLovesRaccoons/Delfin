e21c9a39ff8d60e6a537be5325d7d612ce6e6d4d
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-06 16:56:49 +0100

    mail loads from .env and switches between tls ssl or no encryption

--- php/src/test.php
+++ php/src/test.php
@@ -23,36 +23,63 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-
-// junk code 
-// ! DO NOT USE
-
-try {
-    // Create new instance
-    $mail = new PHPMailer(true);
-
-    // Set up SMTP settings (if needed)
-    $mail->isSMTP();
-    $mail->Host = 'smtp.example.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'user@example.com';
-    $mail->Password = 'password';
-
-    // Send mail
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('to@example.com', 'User');
-
-    $mail->Subject = 'Hello World';
-    $mail->Body = 'This is a test email.';
-
-    if ($mail->send()) {
-        echo 'Email sent successfully.';
-    } else {
-        echo 'Error sending email: ' . $mail->error;
-    }
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ' . $e->getMessage();
+# variables for mail server
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPAuth = filter_var(getenv('SMTP_AUTH'), FILTER_VALIDATE_BOOLEAN);
+if (!$mail->SMTPAuth) {
+    $mail->SMTPSecure = '';
 }
-?>
+else {
+    $tls_or_ssl = strtolower(getenv('SMTP_SECURE'));    // forces lowercase
+    if ($tls_or_ssl == 'tls') {
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    }
+    elseif ($tls_or_ssl == 'ssl') {
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    }
+    else {
+        $mail->SMTPSecure = ''; // this should never happen though
+    }
+}
+
+$mail->Host = getenv('SMTP_SERVER');
+$mail->Username = getenv('SMTP_USERNAME');
+$mail->Password = getenv('SMTP_PASSWORD');
+$mail->Port = getenv('SMTP_PORT');
+
+
+
+
+// // junk code 
+// // ! DO NOT USE
+
+// try {
+//     // Create new instance
+//     $mail = new PHPMailer(true);
+
+//     // Set up SMTP settings (if needed)
+//     $mail->isSMTP();
+//     $mail->Host = 'smtp.example.com';
+//     $mail->SMTPAuth = true;
+//     $mail->Username = 'user@example.com';
+//     $mail->Password = 'password';
+
+//     // Send mail
+//     $mail->setFrom('from@example.com', 'Mailer');
+//     $mail->addAddress('to@example.com', 'User');
+
+//     $mail->Subject = 'Hello World';
+//     $mail->Body = 'This is a test email.';
+
+//     if ($mail->send()) {
+//         echo 'Email sent successfully.';
+//     } else {
+//         echo 'Error sending email: ' . $mail->error;
+//     }
+// } catch (Exception $e) {
+//     echo 'Message could not be sent. Mailer Error: ' . $e->getMessage();
+// }
+// ?>
 
 <?php require 'footer.html'; ?>
