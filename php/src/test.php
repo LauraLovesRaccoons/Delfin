1323081bffc1929d3bb9c3e8afe9836b1e249481
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-06 09:33:28 +0100

    expansions with composer load poperly

--- php/src/test.php
+++ php/src/test.php
@@ -25,7 +25,6 @@ use PHPMailer\PHPMailer\Exception;
 try {
     // Create new instance
     $mail = new PHPMailer(true);
-    echo "$mail <br />";
 
     // Set up SMTP settings (if needed)
     $mail->isSMTP();
