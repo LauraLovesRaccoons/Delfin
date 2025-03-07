4f0fe24e8a9d097ae201e9d26f5f9cacf4b23d5b
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-07 10:02:50 +0100

    email should now send smth

--- php/src/functions.php
+++ php/src/functions.php
@@ -9,6 +9,11 @@ session_name("$session_name");              // now this is the cookie's name
 // Load Composer's autoloader
 require 'vendor/autoload.php';
 
+// required for functions
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 
 function debug_test_env_delfin()
 {
@@ -77,5 +82,45 @@ function session_checker_delfin()
         echo "Welcome: $_SESSION[username] <script>console.log('Welcome: $_SESSION[username]');</script>";
     } else {
         header("location: index.php");  // this requires a session from login
+    }
+}
+
+// HTML ONLY
+function send_mail_delfin($emailSender, $emailSenderName, $emailRecipient, $emailRecipientName, $emailSubject, $emailBody, $emailAttachement)
+{
+    $mail = new PHPMailer(true);    // true enables exceptions
+    try {
+        $encryptionType = strtolower(getenv('SMTP_SECURE'));    // forces lowercase
+        echo "<script>console.log('.env encryption type in lower case: [ $encryptionType ]');</script>";
+        if ($encryptionType == 'tls') {
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            echo "<script>console.log('encryption type: TLS');</script>";
+        } elseif ($encryptionType == 'ssl') {
+            $mail->SMTPAuth = true;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            echo "<script>console.log('encryption type: SSL');</script>";
+        } else {
+            $mail->SMTPAuth = false;
+            $mail->SMTPSecure = ''; // which means unencrypted
+            echo "<script>console.log('encryption type: NONE');</script>";
+        }
+        $mail->Host = getenv('SMTP_SERVER');
+        $mail->Username = getenv('SMTP_USERNAME');
+        $mail->Password = getenv('SMTP_PASSWORD');
+        $mail->Port = getenv('SMTP_PORT');
+        // Recipients
+        $mail->setFrom($emailSender, $emailSenderName);
+        $mail->addAddress($emailRecipient, $emailRecipientName);
+        // Attachments
+        $mail->addAttachment($emailAttachement);
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = $emailSubject;
+        $mail->Body = $emailBody;
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
--- php/src/test.php
+++ php/src/test.php
@@ -19,77 +19,22 @@ require "functions.php";
 <?php
 
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-# variables for mail server
-$mail = new PHPMailer(true);    // true enables exceptions
-$mail->isSMTP();
-// $mail->SMTPAuth = filter_var(getenv('SMTP_AUTH'), FILTER_VALIDATE_BOOLEAN);
-// if (!$mail->SMTPAuth) {
-//     $mail->SMTPSecure = '';
-// }
-// else {
-    debug_test_env_delfin();
-$twssss = getenv('SMTP_SECURE');
-var_dump($twssss);
-$encryptionType = strtolower(getenv('SMTP_SECURE'));    // forces lowercase
-echo "<script>console.log('encryption type: $encryptionType');</script>";
-var_dump($encryptionType);
-if ($encryptionType == 'encryption type: tls') {
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    echo "<script>console.log('TLS');</script>";
-} elseif ($encryptionType == 'encryption type: ssl') {
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    echo "<script>console.log('SSL');</script>";
-} else {
-    $mail->SMTPAuth = false;
-    $mail->SMTPSecure = ''; // which means unencrypted
-    echo "<script>console.log('encryption type: NONE');</script>";
-}
-// }
-
-$mail->Host = getenv('SMTP_SERVER');
-$mail->Username = getenv('SMTP_USERNAME');
-$mail->Password = getenv('SMTP_PASSWORD');
-$mail->Port = getenv('SMTP_PORT');
 
 
-
-
-// // junk code 
-// // ! DO NOT USE
-
-// try {
-//     // Create new instance
-//     $mail = new PHPMailer(true);
-
-//     // Set up SMTP settings (if needed)
-//     $mail->isSMTP();
-//     $mail->Host = 'smtp.example.com';
-//     $mail->SMTPAuth = true;
-//     $mail->Username = 'user@example.com';
-//     $mail->Password = 'password';
-
-//     // Send mail
-//     $mail->setFrom('from@example.com', 'Mailer');
-//     $mail->addAddress('to@example.com', 'User');
-
-//     $mail->Subject = 'Hello World';
-//     $mail->Body = 'This is a test email.';
-
-//     if ($mail->send()) {
-//         echo 'Email sent successfully.';
-//     } else {
-//         echo 'Error sending email: ' . $mail->error;
-//     }
-// } catch (Exception $e) {
-//     echo 'Message could not be sent. Mailer Error: ' . $e->getMessage();
-// }
 // 
+$emailSender = 'test@email.com';
+$emailSenderName = 'TEST WOMAN';
+$emailRecipient = 'receive@email.com';
+$emailRecipientName = 'RECEIVER';
+$emailSubject = 'TEST EMAIL';
+$emailBody = 'This is a test email';
+$emailAttachement = 'favicon.ico';
+// 
+send_mail_delfin($emailSender, $emailSenderName, $emailRecipient, $emailRecipientName, $emailSubject, $emailBody, $emailAttachement);
+
+
+
+
 ?>
 
 <?php require 'footer.html'; ?>
