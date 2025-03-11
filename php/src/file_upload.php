71a90aefd3ea2ff3cef4deb746b11244ded5ba2b
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-11 10:06:48 +0100

    file upload folder is only generated on submit

--- php/src/file_upload.php
+++ php/src/file_upload.php
@@ -10,9 +10,9 @@ session_checker_delfin();
 // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 // $uploadOk = 1;
 
-
-pdf_upload_delfin($file);
-
+if (isset($_POST['submit_button'])) {
+  pdf_upload_delfin($file);
+}
 
 
 
@@ -21,15 +21,19 @@ require 'header.html';
 ?>
 
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="submit">
-</form>
+<br />
+<form method="POST" enctype="multipart/form-data">
 
+  <em>Select pdf file to upload:</em><br />
+  <label for="file"></label>
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <br />
+  <label for="submit"></label>
+  <input type="submit" value="Upload Image" name="submit_button" id="">
+</form>
+<br />
 
 
 <?php require 'footer.html'; ?>
-
 
 
