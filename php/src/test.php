686db15dd8968f32628feaefbd7ac47c8b422bf7
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-14 15:08:01 +0100

    cleanup

--- php/src/test.php
+++ php/src/test.php
@@ -38,55 +38,7 @@ $replacements = [
     '«ERRORRRORRRRR»' => 'AA<AAAAASHBSJBU989965S',
 ];
 
-// Create an instance of the Word class
-// $word = new Word();
-// $word->findAndReplace($templateFile, $outputFile, $replacements);
-
-echo "Document generated successfully: <a href='$outputFile'>Download Here</a>";
+// function here
 
 
-// shell tests
-// echo "<h1>pdf converter test</h1><br />";
-// // 
-// echo shell_exec("whoami");
-// echo "<br />";
-// echo shell_exec("which libreoffice");
-// echo "<br />";
-// // 
 
-
-// function convertDocxToPdf($inputDocx, $outputPdf)
-// {
-//     $inputDocx = escapeshellarg($inputDocx);    // requires real path
-//     $outputPdf = escapeshellarg($outputPdf);    // ditto
-//     if (file_exists($outputPdf)){
-//         unlink($outputPdf);         // it can't overwrite exisiting files
-//     }
-//     // /var/www/html/ is from compose.yaml
-//     $command = "HOME=/tmp libreoffice --headless --convert-to pdf --outdir /var/www/html/uploads $inputDocx 2>&1";
-//     $output = shell_exec($command);
-
-//     // file_put_contents('/var/www/html/uploads/convert_log.txt', $output);    // logging file
-//     // echo "<pre>$output</pre>";  // visible on the webpage
-//     // echo "<br />";  // 
-
-//     return file_exists($outputPdf) ? $outputPdf : false;
-// }
-
-
-$docxFile = "./uploads/output.docx";
-$pdfFile = "./uploads/output.pdf";
-// $docxFile = realpath("./uploads/output.docx");
-// $pdfFile = realpath("./uploads/output.pdf");
-$inputDocx = "./uploads/testdocx.docx";
-$outpdfFile = "./uploads/AAAAA/HI_output.pdf";
-
-// convertDocxToPdf($docxFile, $pdfFile);
-convertDocxToPdf($inputDocx, $outpdfFile);
-
-
-// if (convertDocxToPdf($docxFile, $pdfFile)) {
-//     echo "Conversion successful: $pdfFile";
-// } else {
-//     echo "Conversion failed.";
-// }
