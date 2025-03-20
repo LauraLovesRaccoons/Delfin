3e2ca36e3f145b30cea8d8a3fd18cf484a4feb2f
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-20 11:26:12 +0100

    Update superjhemp.php

--- php/src/superjhemp.php
+++ php/src/superjhemp.php
@@ -350,5 +350,4 @@
 
 
 <!-- I need about tree fiddy -->
-<!-- I ain't giving you no treefiddy you goddam Loch Ness monster! Get your own goddam money! -->
- 
+<!-- I ain't giving you no tree fiddy you goddam Loch Ness monster! Get your own goddam money! -->
