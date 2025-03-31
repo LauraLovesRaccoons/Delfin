cb6c14ea4228bbe320e549d3aab079fe3077b85b
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-31 18:03:45 +0200

    Update signature_template.php

--- php/src/functions/signature_template.php
+++ php/src/functions/signature_template.php
@@ -20,21 +20,25 @@ $signatureName = $_SESSION['signatureName'];
 $signaturePhone = $_SESSION['signaturePhone'];
 $signatureService = $_SESSION['signatureService'];
 $signatureEmail = $_SESSION['email'];   // the email is universal
-//! optional vars will check if string is bigger then 1 and then load the string with the variable
+$signatureTitle = $_SESSION['signatureTitle'];
+$signatureOffice = $_SESSION['signatureOffice'];
+$signatureGSM = $_SESSION['signatureGSM'];
+$signatureFax = $_SESSION['signatureFax'];
+// optional vars will check if string is bigger then 1 and then load the string with the variable
 //? optional vars
-$signatureTitle = "'<br><i>{#title}</i>'";
-$sigantureOffice = "'<br><i>{#office}</i>'";
-$sigantureGSM = "<br><i>GSM: {#mobile}</i>";
-$sigantureFax = "<br><i>Fax: {#fax}</i>";
+$signatureTitle = (strlen($signatureTitle) <  2) ? '' : '<br><i>' . $signatureTitle. '</i>';
+$sigantureOffice = (strlen($sigantureOffice) <  2) ? '' : '<br><i>' . $sigantureOffice. '</i>';
+$sigantureGSM = (strlen($sigantureGSM) <  2) ? '' : '<br><i>GSM: ' . $sigantureGSM. '</i>';
+$sigantureFax = (strlen($sigantureFax) <  2) ? '' : '<br><i>Fax: ' . $sigantureFax. '</i>';
 // banner
-$banner = "";     //? leaving this empty for now (or forever?)
+$banner = '';   //? leaving this empty for now (or forever?)
 
 //! template
 $template = 
     '<br />' . 
     '<table border=1 cellspacing=0 cellpadding=0 style="border-collapse:collapse;border:none;"><tr>' . 
     '<td width="110" valign=top align=center style="border:solid black 1.0pt;border-right:none;padding:15px 8px 15px 8px;">' . 
-    '<p><img width=94 height=58 src="http://web.petange.lu/signature/email.logo.jpg" alt="Logo P&eacute;tange"></p></td>' . 
+    '<p><img width=94 height=58 src="https://web.petange.lu/signature/email.logo.jpg" alt="Logo P&eacute;tange"></p></td>' . 
     '<td width="300" valign=top style="border:solid black 1.0pt;border-left:none;padding:15px 5px 15px 5px;">' . 
     '<span style="font-family:Calibri;color:#1f497d;font-size:10pt;">' . 
     '<b><span style="color:#101010;">' . $signatureName . '</span></b>' . 
