ec2174fa25c202a2ba3fa696b0b8c75f743d8cd4
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-03-31 16:43:59 +0200

    Update signature_template.php

--- php/src/functions/signature_template.php
+++ php/src/functions/signature_template.php
@@ -16,17 +16,42 @@ if (!isset($_SESSION['signatureName'], $_SESSION['signaturePhone'], $_SESSION['s
 
 
 //? template vars
-$user['signatureName'] = $_SESSION['signatureName'];
-$user['signaturePhone'] = $_SESSION['signaturePhone'];
-$user['signatureService'] = $_SESSION['signatureService'];
+$signatureName = $_SESSION['signatureName'];
+$signaturePhone = $_SESSION['signaturePhone'];
+$signatureService = $_SESSION['signatureService'];
+$signatureEmail = $_SESSION['email'];   // the email is universal
+//! optional vars will check if string is bigger then 1 and then load the string with the variable
+//? optional vars
+$signatureTitle = "'<br><i>{#title}</i>'";
+$sigantureOffice = "'<br><i>{#office}</i>'";
+$sigantureGSM = "<br><i>GSM: {#mobile}</i>";
+$sigantureFax = "<br><i>Fax: {#fax}</i>";
+// banner
+$banner = "";     //? leaving this empty for now (or forever?)
 
 //! template
-$template = "<br /> SIGNATURE IS POSSIBLE <br />" .
-    "<br />" .
-    "<strong>" . $user['signatureName'] . "</strong><br />" .
-    "<strong>" . $user['signaturePhone'] . "</strong><br />" .
-    "<strong>" . $user['signatureService'] . "</strong><br />" .
-    "<br />FAREWELL";
+$template = 
+    '<br />' . 
+    '<table border=1 cellspacing=0 cellpadding=0 style="border-collapse:collapse;border:none;"><tr>' . 
+    '<td width="110" valign=top align=center style="border:solid black 1.0pt;border-right:none;padding:15px 8px 15px 8px;">' . 
+    '<p><img width=94 height=58 src="http://web.petange.lu/signature/email.logo.jpg" alt="Logo P&eacute;tange"></p></td>' . 
+    '<td width="300" valign=top style="border:solid black 1.0pt;border-left:none;padding:15px 5px 15px 5px;">' . 
+    '<span style="font-family:Calibri;color:#1f497d;font-size:10pt;">' . 
+    '<b><span style="color:#101010;">' . $signatureName . '</span></b>' . 
+    '<br><i>Administration communale de Pétange</i>' . 
+    '<br><i>' . $signatureService . '</i>' . 
+    $signatureTitle . 
+    $sigantureOffice . 
+    '<br><i>T&eacute;l: ' . $signaturePhone . '</i>' . 
+    $sigantureGSM . 
+    $sigantureFax . 
+    '<br><i>E-Mail&nbsp;:&nbsp;<a href="mailto:' . $signatureEmail . '">' . $signatureEmail . '</a></i>' . 
+    '<br><i>Web&nbsp;:&nbsp;</i><a href="https://www.petange.lu"><i>https://www.petange.lu</i></a>' . 
+    '</span></td></tr>' . 
+    $banner . 
+    '<br />'
+    ;
+
 
 
 // function to append the filled in template
