a265a8376ff134514d9cdbb430a462d2936c44f3
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-04-02 08:51:29 +0200

    disable PMA now just disables pma service - no restart

--- batch files/disable_PMA.bat
+++ batch files/disable_PMA.bat
@@ -1,11 +1,8 @@
 @ECHO OFF
 ECHO Disable phpMyAdmin
-ECHO The Entire Docker Container Will Restart
 ECHO --------------------------------
 cd ../
-docker compose --profile pma stop
-timeout /t 10
-docker compose up -d
-ECHO Docker Job Complete
+docker compose stop phpmyadmin
 ECHO --------------------------------
+ECHO Docker Job Complete
 PAUSE
