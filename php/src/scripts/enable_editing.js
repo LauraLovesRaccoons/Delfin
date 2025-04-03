ee22dfa4b78201a545e60da5795b7a461a614965
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-04-03 14:06:56 +0200

    unlock button now doesm't reload the js for eacj element

--- php/src/scripts/enable_editing.js
+++ php/src/scripts/enable_editing.js
@@ -5,29 +5,32 @@ editButton.addEventListener("click", function () {
 
   // I'm append a new class but keep the old one as well for stylesheet reasons
   document.querySelectorAll(".kick-symbol").forEach((element) => {
-    // element.classList.add("kick-symbol_edit");
-    // 
-    const script = document.createElement('script');
-    script.src = './scripts/edit_kick.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
+    element.classList.add("kick-symbol_edit");
+    //
   });
+
+  const script_edit_kick = document.createElement("script");
+  script_edit_kick.src = "./scripts/edit_kick.js";
+  script_edit_kick.type = "text/javascript";
+  document.head.appendChild(script_edit_kick);
 
   document.querySelectorAll(".table_text").forEach((element) => {
-    // element.classList.add("table_text_edit");
-    // 
-    const script = document.createElement('script');
-    script.src = './scripts/edit_text.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
+    element.classList.add("table_text_edit");
+    //
   });
 
+  const script_edit_text = document.createElement("script");
+  script_edit_text.src = "./scripts/edit_text.js";
+  script_edit_text.type = "text/javascript";
+  document.head.appendChild(script_edit_text);
+
   document.querySelectorAll(".table_tinyint").forEach((element) => {
-    // element.classList.add("table_tinyint_edit");
-    // 
-    const script = document.createElement('script');
-    script.src = './scripts/edit_tinyint.js';
-    script.type = 'text/javascript';
-    document.head.appendChild(script);
+    element.classList.add("table_tinyint_edit");
+    //
   });
+
+  const script_edit_tinyint = document.createElement("script");
+  script_edit_tinyint.src = "./scripts/edit_tinyint.js";
+  script_edit_tinyint.type = "text/javascript";
+  document.head.appendChild(script_edit_tinyint);
 });
