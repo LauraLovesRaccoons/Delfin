f8d3f9d1c916853212865e7ebcc0bf818cd7f5a0
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-05-20 08:51:39 +0200

    V1.5.9 ready for release

--- php/src/collision.php
+++ php/src/collision.php
@@ -15,7 +15,7 @@ cleanup_session_vars_delfin();  // this is probably a good idea if someone uses 
 
 <div class="general-wrapper">
     <br />
-    <h1 class="landing-page-h1">You are currently logged in somewhere else and did just submit a job and for safety reasons this file upload has been canceled. The other job is already running and can't be stopped.</h1>
+    <h1 class="landing-page-h1">You are currently logged in somewhere else and you did just submit a job and for safety reasons this file upload has been canceled.<br />The other job is already running and can't be stopped.</h1>
     <h2 class="landing-page-h1"><a class="above20MB" href="delfin.php">Go Back to the Main Page</a></h2>
 </div>
 <?php require 'footer.php'; ?>
