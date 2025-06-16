1263c35fc538665d51051cba38bc12e71eeeafa2
LauraLovesRaccoons <145540895+LauraLovesRaccoons@users.noreply.github.com>
2025-06-16 11:59:23 +0200

    V1.8.3.1

--- php/src/functions/signature_template.php
+++ php/src/functions/signature_template.php
@@ -69,7 +69,7 @@ function appendSignature_delfin($emailBody)
 {
     global $template;   //? i need to call it with global
     // $emailBody = $emailBody . "<br />" . $template;     //? catenates both strings into one ; with a line break
-    $emailBody = '<p style="margin-bottom:1.5rem;">' . $emailBody . '</p>' . $template;   //? catenates both strings into one ; and the message is its own paragraph
+    $emailBody = '<p style="margin-bottom:24px;">' . $emailBody . '</p>' . $template;   //? catenates both strings into one ; and the message is its own paragraph
     return $emailBody;
 };
 ?>
